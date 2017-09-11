23a020c1f8f0d5c2ac3f1dc947a747a36d63d71f
Will Taylor <w1ll100@hotmail.co.uk>
2017-09-11 04:27:33 +0100

    Things

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from math import ceil
 from data_and_enums import *
 
@@ -42,8 +43,8 @@ class GUI(tk.Tk):
 				r = 0
 				c = 1
 
-		self.check_all_classes_button = tk.Button(self.class_setting_frame, text = "Check all", command = lambda : setAllChecks(self.class_setting_frame, True))
-		self.uncheck_all_classes_button = tk.Button(self.class_setting_frame, text = "Uncheck all", command = lambda : setAllChecks(self.class_setting_frame, False))
+		self.check_all_classes_button = tk.ttk.Button(self.class_setting_frame, text = "Check all", command = lambda : setAllChecks(self.class_setting_frame, True))
+		self.uncheck_all_classes_button = tk.ttk.Button(self.class_setting_frame, text = "Uncheck all", command = lambda : setAllChecks(self.class_setting_frame, False))
 		self.check_all_classes_button.grid(row = max_rows + 1, column = 0)
 		self.uncheck_all_classes_button.grid(row = max_rows + 1, column = 1)
 
@@ -65,8 +66,8 @@ class GUI(tk.Tk):
 				r = 0
 				c = 1
 
-		self.check_all_racees_button = tk.Button(self.race_setting_frame, text = "Check all", command = lambda : setAllChecks(self.race_setting_frame, True))
-		self.uncheck_all_racees_button = tk.Button(self.race_setting_frame, text = "Uncheck all", command = lambda : setAllChecks(self.race_setting_frame, False))
+		self.check_all_racees_button = tk.ttk.Button(self.race_setting_frame, text = "Check all", command = lambda : setAllChecks(self.race_setting_frame, True))
+		self.uncheck_all_racees_button = tk.ttk.Button(self.race_setting_frame, text = "Uncheck all", command = lambda : setAllChecks(self.race_setting_frame, False))
 		self.check_all_racees_button.grid(row = max_rows + 1, column = 0)
 		self.uncheck_all_racees_button.grid(row = max_rows + 1, column = 1)
 
@@ -75,7 +76,7 @@ class GUI(tk.Tk):
 
 		self.ability_mode = tk.StringVar()
 		self.ability_mode.set("default")
-		self.ability_mode.trace("w", self.toggleAbilityModeEnabled)
+		self.ability_mode.trace("w", self.toggleCustomAbilityModeEnabled)
 		modes = [("Class default", "default"),
 					("Custom", "custom")]
 
@@ -85,12 +86,18 @@ class GUI(tk.Tk):
 			ability_radiobutton.grid(column = 0, row = r, sticky = tk.W)
 			r += 1
 
+		attribute_label = tk.Label(self.ability_setting_frame, text = "Attribute")
+		priority_label = tk.Label(self.ability_setting_frame, text = "Priority")
+		attribute_label.grid(column = 0, row = r)
+		priority_label.grid(column = 1, row = r)
+		r += 1
+
 		self.ability_var_dict = dict((s, tk.IntVar()) for s in Abilities)
 		self.ability_priority_menus = []
 
 		for ability in Abilities:
 			self.ability_var_dict[ability].set(1)
-			drop_menu = tk.OptionMenu(self.ability_setting_frame, self.ability_var_dict[ability], 1, 2, 3, 4, 5, 6)
+			drop_menu = tk.ttk.OptionMenu(self.ability_setting_frame, self.ability_var_dict[ability], None, 1, 2, 3, 4, 5, 6)
 			drop_menu.configure(state = "disabled")
 			self.ability_priority_menus.append(drop_menu)
 			label = tk.Label(self.ability_setting_frame, text = ability.value)
@@ -100,18 +107,21 @@ class GUI(tk.Tk):
 
 	def setUpFinalFrame(self):
 		self.final_setting_frame = tk.Frame(self)
-		self.unrestricted_char_button = tk.Button(self.final_setting_frame, text = "Create unrestricted character")
-		self.restricted_char_button = tk.Button(self.final_setting_frame, text = "Create \"sensible\" character")
+		self.unrestricted_char_button = tk.ttk.Button(self.final_setting_frame, text = "Create unrestricted character", command = lambda : self.collectOptionsAndCreateCharacter("unrestricted"))
+		self.restricted_char_button = tk.ttk.Button(self.final_setting_frame, text = "Create \"sensible\" character", command = lambda : self.collectOptionsAndCreateCharacter("restricted"))
 		self.unrestricted_char_button.grid(column = 0, row = 0)
 		self.restricted_char_button.grid(column = 0, row = 1)
 
-	def toggleAbilityModeEnabled(self, *args):
+	def toggleCustomAbilityModeEnabled(self, *args):
 		if self.ability_mode.get() == "default":
 			for menu in self.ability_priority_menus:
 				menu.configure(state = "disabled")
 		else:
 			for menu in self.ability_priority_menus:
 				menu.configure(state = "active")
+
+	def collectOptionsAndCreateCharacter(self, mode):
+		print(mode)
 
 
 if __name__ == "__main__":
